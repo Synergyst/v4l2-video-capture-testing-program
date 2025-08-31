c57cb9fb4026ddbcaf5e4254394faaa45b034524
Synergyst <root@mail.synergyst.club>
2025-08-31 18:22:24 -0500

    Actually fix v4l2-ctl setup script dependency.

--- runner-video.sh
+++ runner-video.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-./v4l2-capture-test --devices /dev/video0 --port 1337 --fps 60 --mjpeg --bgr --jpeg-quality 70 --encode-threads 8 --lazy --lazy-threshold 600
+
+cd /root/v4l2-video-capture-testing-program
+/root/v4l2-video-capture-testing-program/v4l2-capture-test --devices /dev/video0 --port 1337 --fps 60 --mjpeg --bgr --jpeg-quality 50 --encode-threads 12 --lazy --lazy-threshold 600
 
 exit 0
